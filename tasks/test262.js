731a5fd71bf5b0d4c81c529d853727a2f8485c2c
Joshua Searles <jrsearles@comcast.net>
2015-09-29 18:06:15 -0400

    Implementation of destructuring assignments

--- tasks/test262.js
+++ tasks/test262.js
@@ -48,7 +48,7 @@ gulp.task("test262-6", function () {
 	// types +
 	// white-space +
 
-	return streamer6({ files: ["/language/expressions/**/*.js"] })
+	return streamer6({ files: ["/language/expressions/assignment/**/*.js"] })
 		.pipe(through.obj(function (file, enc, cb) {
 			let filename = path.basename(file.path);
 
